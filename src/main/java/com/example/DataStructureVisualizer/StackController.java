af4da7e02199ee4b8229e555d446c410f1fdf540
deadshoot333 <arqam343almas@gmail.com>
2023-09-28 14:07:01 +0600

    Queue added but cannot visualize

--- src/main/java/com/example/DataStructureVisualizer/StackController.java
+++ src/main/java/com/example/DataStructureVisualizer/StackController.java
@@ -1,6 +1,8 @@
 package com.example.DataStructureVisualizer;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -11,6 +13,7 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Stack;
 
 public class StackController {
@@ -28,21 +31,22 @@ public class StackController {
     private final Stack<StackPane> stack = new Stack<>();
     private Stage stage;
     private Scene scene;
+
     public void initialize() {
         pushButton.setOnAction(event -> pushSquare());
         popButton.setOnAction(event -> popSquare());
     }
 
-    private void pushSquare()   {
+    private void pushSquare() {
         Rectangle rectangle = new Rectangle(30, 30);
         rectangle.setFill(Color.WHITE);
         //rectangle.setX(stackPane.getWidth());
         //rectangle.setY(stackPane.getHeight());
-        int data=Integer.parseInt(textField.getText());
-        String my_string=Integer.toString(data);
-        Text text=new Text(my_string);
-        StackPane stackpane=new StackPane();
-        stackpane.getChildren().addAll(rectangle,text);
+        int data = Integer.parseInt(textField.getText());
+        String my_string = Integer.toString(data);
+        Text text = new Text(my_string);
+        StackPane stackpane = new StackPane();
+        stackpane.getChildren().addAll(rectangle, text);
         stack.push(stackpane);
         //stackPane.getChildren().add(rectangle);
 
