2a7d28f478d50ca4132818fdfe8dd52a0734ac6a
deadshoot333 <arqam343almas@gmail.com>
2023-10-08 16:57:22 +0600

    Binary Tree codes added cannot animate

--- src/main/java/com/example/DataStructureVisualizer/Main.java
+++ src/main/java/com/example/DataStructureVisualizer/Main.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("mainScene.fxml"));
@@ -18,6 +19,14 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        BinaryTree b=new BinaryTree();
+        b.insert(18);
+        b.insert(1);
+        b.insert(19);
+        b.insert(10);
+        b.insert(5);
+        b.insert(15);
+        b.print();
+        //launch();
     }
 }
